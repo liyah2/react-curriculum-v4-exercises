a4dcfe40e0223f74575aba449c0311aebcb504f2
liyah2 <aaliyahcloss@gmail.com>
2026-05-19 18:58:44 -0400

    Creating API request

--- src/exercises/lesson-07/api.js
+++ src/exercises/lesson-07/api.js
@@ -30,6 +30,7 @@ export function getPosts() {
   // You may delete this comment once you've finished the implementation.
   // eslint-disable-next-line no-unused-vars
   const url = POSTS_ENDPOINT;
+  return fetch(url).then((response) => response.json());
 }
 
 /**
